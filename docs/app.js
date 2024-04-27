b12841f3e416182c25eaf7c97f14b182cd67ec78
Borislav Angelov <98764167+b-angelov@users.noreply.github.com>
2024-04-27 20:14:46 +0300

    updating pages

--- docs/app.js
+++ docs/app.js
@@ -1,11 +1,38 @@
-function logics(){
+function logics() {
     const repositoryApiUrl = "https://api.github.com/repos/b-angelov/softuni-exercises/contents"
+    navigation
+    const availableLanguages = {
+        python: {
+            fontIcon: "",
+        },
+        js: {
+            fontIcon: ""
+        },
+        javascript: ["js"],
+        html: {
+            fontIcon: ""
+        },
+        css: {
+            fontIcon: ""
+        },
+        db: {
+            fontIcon: ""
+        },
+        sql: {
+            fontIcon: ""
+        },
+        postgre: {
+            fontIcon: ""
+        }
+    }
 
-    async function getDirectoryTree(subPath=""){
+    async function getDirectoryTree(subPath = "") {
         dirTree = await fetch(`${repositoryApiUrl}/${subPath}`)
         dirTree = await dirTree.json()
         console.log(dirTree)
     }
+
     getDirectoryTree()
 }
+
 logics()
