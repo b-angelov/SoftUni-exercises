2908aa63ba93361d766c302289e9ce968bc2abba
Borislav Angelov <98764167+b-angelov@users.noreply.github.com>
2024-04-27 13:16:51 +0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,10 @@
 function logics(){
-    console.log(_dirname)
+    const repositoryApiUrl = "https://api.github.com.repos/b-angelov/softuni-exercises/contents"
+
+    async function getDirectoryTree(subPath=""){
+        dirTree = await fetch(`${repositoryApiUrl}/${subPath}`)
+        dirTree = await dirTree.json()
+        console.log(dirTree)
+    }
 }
 logics()
